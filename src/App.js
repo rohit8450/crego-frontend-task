e9260a17455f28bf6af29d75fea9cde7220feb33
rohit8450 <rohitrajthakur10@gmail.com>
2024-01-27 15:01:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpressionEngine from "./Component/ExpressionEngine";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="pb-5 pt-3 bg-dark">
+      <h2 className="text-warning text-center mt-4 mb-5">Expression Form Engine</h2>
+      <ExpressionEngine />
     </div>
   );
 }
